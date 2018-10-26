7131523249a191d19dabb8f4bb8cd12284d1419d
gali4 <gali3@uh.edu>
2018-10-26 16:55:52 -0500

    updated

--- curl.php
+++ curl.php
@@ -1,82 +1,61 @@
 <?php
-  $token = "b6861aae0cd7440e9aa95006a0fd751a";
-  $method = $_GET['method'];
-  $pin = $_GET['pin'];
-  $state = $_GET['state'];
-//   // http://blynk-cloud.com/4ae3851817194e2596cf1b7103603ef8/update/D8?value=1
-  // Create cURL call
-  if ($method === "update")
-  {
-    // session_destroy();
-  $service_url = 'http://blynk-cloud.com/' . $token. '/' . $method . '/' . $pin . '?value=' . $state;
-  // $service_url = 'http://blynk-cloud.com/' . $token. '/update/D13?value=255';
-  }
+// header('Content-Type: text/plain');
+$API = "b6861aae0cd7440e9aa95006a0fd751a";
+// include 'config/keys.php';
+$pin  = $_GET['pin'];
+$type = $_GET['method'];
+$url  = "http://blynk-cloud.com";
+// $url = 'https://uhouston-csm.symplicity.com/api/public/v1/calendar-events?category=workshop&dateRange=["2015-05-22","2019-09-30"]';
 
-else{
-  // session_destroy();
-  $service_url = 'http://blynk-cloud.com/' . $token . '/' . $method . '/' . $pin;
-  // $service_url = 'http://blynk-cloud.com/' . $token . '/get/D13';
+// $curlUrl = "$url/$API/$type/$pin";
+$curl = curl_init();
 
+curl_setopt_array($curl, array(
+    // $service_url = $url . '?category=' . $category . '&dateRange=' . $dateRange;
+
+    CURLOPT_URL            => "$url/$API/$type/$pin",
+    // CURLOPT_URL            => 'http://blynk-cloud.com/b6861aae0cd7440e9aa95006a0fd751a/get/D13',
+
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING       => "",
+    CURLOPT_MAXREDIRS      => 10,
+    CURLOPT_TIMEOUT        => 30,
+    CURLOPT_SSL_VERIFYHOST => 0,
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST  => "GET",
+    CURLOPT_HTTPHEADER     => array('Accept: application/json'),
+    CURLOPT_HTTPHEADER     => array('Content-Type: application/json'),
+    // CURLOPT_HTTPHEADER => array(
+    //   "Authorization: Token dMOTV8odgjukfLRsfzEWpwchLxmuYze6joJYWtOTL6q6d3CoPHi0CISkswPM92i74iaOP8fN12khUa7jCKObv07jfCmE5OdUTywAeuGWIcBoxFGTKiJgGuUTUTjvCHqnIOE0kSwzw8s=",
+    //   "Cache-Control: no-cache",
+    //   "Postman-Token: 2fa1484b-e109-4f87-881a-acda09aa0b01"
+    // ),
+));
+
+$response = curl_exec($curl);
+// $json     = json_decode($response, true);
+
+$err = curl_error($curl);
+
+curl_close($curl);
+function removeslashes($string)
+{
+    $string = implode("", "", explode("[", "]", $string));
+    return stripslashes(trim($string));
 }
-  $curl = curl_init($service_url);
-   
-  // Send cURL to Yun board
-  curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4); 
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
-  // global $curl_response;
-
-  $curl_response = curl_exec($curl);
-
-  curl_close($curl);
-
-  //Print answer
-  // echo $curl_response;
-  session_start();
-  $_SESSION['response'] = $curl_response;
-  // return $curl_response
-  // function ($curl_response){
-  // return $curl_response;
-  // }
- 
-
-// // Get cURL resource
-// $curl = curl_init();
-// // Set some options - we are passing in a useragent too here
-// curl_setopt_array($curl, array(
-//     CURLOPT_RETURNTRANSFER => 1,
-//     CURLOPT_URL => "http://blynk-cloud.com/' . $token . '/' . $method . '/' . $pin",
-//     CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-// ));
-// // Send the request & save response to $resp
-// $resp = curl_exec($curl);
-// echo $resp;
-// // Close request to clear up some resources
-// curl_close($curl);
-
-
-// Get cURL resource
-// $curl = curl_init();
-// $url = "http://blynk-cloud.com";
-// // Set some options - we are passing in a useragent too here
-// curl_setopt_array($curl, array(
-//     CURLOPT_RETURNTRANSFER => 1,
-//     CURLOPT_URL => "$url/$token/$method/$pin?value=$state",
-//     CURLOPT_USERAGENT => 'Codular Sample cURL Request',
-//     CURLOPT_POST => 1,
-//     // CURLOPT_POSTFIELDS => array(
-//     //            value => $pin
-
-//     // )
-// ));
-// // Send the request & save response to $resp
-// $resp = curl_exec($curl);
-// // Close request to clear up some resources
-// curl_close($curl);
-
-
-// session_start();
-// $_SESSION['response'] = $rsp;
-
-
-
-?>
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    // session_start();
+    // $_SESSION['response'] = $curl_response;
+    // $response   = stripslashes($response);
+    // $json_array = json_decode($response, true);
+    // print_r($json_array);
+    // $result = stripslashes_deep($response);
+    // print_r($result);
+    $stringWithoutNonLetterCharacters   = preg_replace("/[\/\[\\]\&%#\$]/", "", $response);
+    $stringWithQuotesReplacedWithSpaces = preg_replace("/[\"\']/", "", $stringWithoutNonLetterCharacters);
+    // echo removeslashes($response);
+    print_r($stringWithQuotesReplacedWithSpaces);
+}
--- interface.php
+++ interface.php
@@ -1,132 +1,73 @@
-
-<?php
+<!-- <?php
 session_start();
-$test = "hello";
-$token = "b6861aae0cd7440e9aa95006a0fd751a";
-  $method = $_GET['method'];
-  $pin = $_GET['pin'];
-  $state = $_GET['state'];
-  
-//   // http://blynk-cloud.com/4ae3851817194e2596cf1b7103603ef8/update/D8?value=1
-  // Create cURL call
-  if ($method === "update")
-  {
-    // session_destroy();
-  $service_url = 'http://blynk-cloud.com/' . $token. '/' . $method . '/' . $pin . '?value=' . $state;
-  // $service_url = 'http://blynk-cloud.com/' . $token. '/update/D13?value=255';
-  }
+$response = $_SESSION['response'];
+?> -->
 
-else{
-  // session_destroy();
-  $service_url = 'http://blynk-cloud.com/' . $token . '/' . $method . '/' . $pin;
-  // $service_url = 'http://blynk-cloud.com/' . $token . '/get/D13';
-
-}
-  $curl = curl_init($service_url);
-   
-  // Send cURL to Yun board
-  curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4); 
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
-
-  $curl_response = curl_exec($curl);
-  $_SESSION['response'] = $curl_response;
-//   global $response;
-//   $response = $_SESSION['response'];
-//   echo $response;
-
-  curl_close($curl);
-?>
-
-<script>
-// $(document).ready(function(){
-//     $("#3").click(function($response){
-//             alert("Data: "+ $response);
-//     });
-// });
-var values = "<?php echo $state; ?>";
-function buttonClick(clicked_id) {
-  if (clicked_id == "1") {
-    $.get("interface.php", {
-      pin: "D13",
-      state: "255",
-      method: "update"
-    });
-    $("#val").text(values);
-
-  }
-
-  if (clicked_id == "2") {
-    $.get("interface.php", {
-      pin: "D13",
-      state: "0",
-      method: "update"
-    });
-    // $("#val").text({$test});
-
-  }
-
-  if (clicked_id == "3") {
-    $.get("interface.php", {
-      pin: "D13",
-      //   state: "1",
-      method: "get"
-    });
-
-  }
-}
-
-// });
-
-
-</script>
 </html>
 
 <!DOCTYPE html>
 <html>
 
 <head>
+
     <LINK href="style.css" rel="stylesheet" type="text/css" />
 
-    <script type="text/javascript" src="jquery-2.0.3.min.js"></script>
+    <script type="text/javascript" src="jquery-2.0.3.min.js">
+    </script>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
 
-    
-    
-    <title>Page Title</title>
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js">
+    </script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js">
+    </script>
+
+    <script type="text/javascript" src="script.js">
+    </script>
+
+
+
+    <title>Calendar API</title>
 </head>
 
 <body>
-    <div class="mainContainer">
+    <div class="container">
 
-        <div class="buttonBlock"><span class="buttonTitle">LED 1</span>
-            <button class="button" type="button" id="1" onClick="buttonClick(this.id,)">On</button>
-            <button class="button" type="button" id="2" onClick="buttonClick(this.id)">Off</button>
+        <div class="row">
+            <label for="selectPin">Select Category </label>
 
+            <select name="" id="selectPin">
+                <option value="D13">D13</option>
+
+                <option value="V2">V2</option>
+                <option></option>
+            </select>
+        </div>
+        <div class="row">
+            <label class="col-sm-12">Total Results: <span id="val"></span></label>
+            <label class="col-sm-12">Pin Live Value: <span id="D13val"></span></label>
+            <br><button class="button" type="button" id="start" onClick="send(this.id)">start</button>
+            <!-- <br><button class="button" type="button" id="stop" onClick="buttonClick(this.id)">Submit</button> -->
+
+            <label for="sensor_1_val" class="col-sm-6">Sensor 1 value: <span id="sensor_1_val" value="V2"></span></label>
+            <label for="sensor_2_val" class="col-sm-6">Sensor 2 value: <span id="sensor_2_val" value="V4"></span></label>
+            <label for="alert" class="col-sm-12">Alert: <span id="alert"></span></label>
         </div>
 
-        <div class="buttonBlock"><span class="buttonTitle">LED 2</span>
 
-            <button class="button" type="button" id="3" onClick="buttonClick(this.id)">Get 13</button>
-            <!-- <button class="ledButton" type="button" id="4" onClick="buttonClick(this.id)">Off</button> -->
 
-        </div>
-        <span id="val"><?php echo $response; ?></span>
+
+
+        <!-- <label>Total Results: <span id="dateval"></span></label> -->
+
+
+        <br><button class="button" type="button" id="category" onClick="buttonClick(this.id)">Submit</button>
 
     </div>
 
 </body>
+<script>
+
+</script>
+
 </html>
-
-
-
-
-
-<!-- <script>
-    var test = "<?php echo $test; ?>";
-
-
-    $("#3").click(function(){
-
-$("#val").text(test);
-
-    });
-</script> -->
