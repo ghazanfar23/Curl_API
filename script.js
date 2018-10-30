bad6287e515434e02af6b8f5c512b3803d5599ee
gali4 <gali3@uh.edu>
2018-10-30 13:26:54 -0500

    updated

--- script.js
+++ script.js
@@ -1,133 +1,105 @@
-// Function to control LEDs
-// session_start();
-// var interval
-
-function buttonClick(clicked_id) {
-  if (clicked_id == "category") {
-    var pin = $("#selectPin").val()
-
-    // $("#dateval").text(startDate)
-    var result = null
-    //  var scriptUrl = "somefile.php?name=" + name;
-    $.ajax({
-      url: "curl.php",
-      type: "get",
-      // dataType: "html",
-      async: false,
-      data: { pin: pin, method: "get" },
-      success: function(data) {
-        result = data
-        setTimeout(function() {
-          buttonClick()
-        }, 10000)
-      },
-      // complete: function(data) {
-      //   // Schedule the next
-      // },
-    })
-
-    // alert(result)
-    $("#val").text(result)
-    // console.log(data)
-  }
-}
+// function buttonClick(clicked_id) {
+//   if (clicked_id == "category") {
+//     var pin = $("#selectPin").val()
+//     var result = null
+//     $.ajax({
+//       url: "curl.php",
+//       type: "get",
+//       async: false,
+//       data: { pin: pin, method: "get" },
+//       success: function(data) {
+//         result = data
+//         setTimeout(function() {
+//           buttonClick()
+//         }, 10000)
+//       },
+//     })
+//     $("#val").text(result)
+//   }
+// }
 $(document).ready(function() {
-  // function startClick(clicked_id) {
-
   function send(clicked) {
     var pin = $("#selectPin").val()
-    // if (clicked == "start") {
-
-
-    // $("#dateval").text(startDate)
     var result = null
-    //  var scriptUrl = "somefile.php?name=" + name;
+    $.ajax({
+      url: "curl.php",
+      type: "get",
+      async: false,
+      data: { pin: pin, method: "get" },
+      success: function(data) {
+        result = data
+      },
+    })
+    $("#pinval").text(result)
+
+    var sensor_1 = $("#sensor_1_val").val()
+
+    var result = null
     $.ajax({
       url: "curl.php",
       type: "get",
       // dataType: "html",
       async: false,
-      data: { pin: pin, method: "get" },
-      success: function(data) {
-        result = data
-
-        // setTimeout(function() {
-        //   send()
-        // }, 2000)
+      data: { pin: "V2", method: "get" },
+      success: function(data_1) {
+        result_1 = data_1
       },
     })
-    $("#D13val").text(result)
+    $("#sensor_1_val").text(result_1)
 
-  
-  // send()
-  // function sensor_1() {
+    var sensor_2 = $("#sensor_2_val").val()
 
-  var pin_2 = $("#sensor_1_val").val()
-
-  var result = null
-  //  var scriptUrl = "somefile.php?name=" + name;
-  $.ajax({
-    url: "curl.php",
-    type: "get",
-    // dataType: "html",
-    async: false,
-    data: { pin: "V2", method: "get" },
-    success: function(data_1) {
-      result_1 = data_1
-      if ((result_1 > "30") && (result_1 < "50")) {
-        $("#alert").text("Hit");
+    var result = null
+    $.ajax({
+      url: "curl.php",
+      type: "get",
+      async: false,
+      data: { pin: "V4", method: "get" },
+      success: function(data_2) {
+        result = data_2
+      },
+    })
+    $("#sensor_2_val").text(result)
+    setTimeout(function() {
+      send()
+    }, 50)
+    if (
+      (result_1 > "30" && result_1 < "50") ||
+      (result > "30" && result < "50")
+    ) {
+      if (result > "30" && result < "50") {
+        $("#alert").text("Detected Sensor 2")
+        $("#detection").removeClass("card-light")
+        $("#alert").css("color", "white")
+        $("#detection").addClass("card-success")
       }
-      else{
-        $("#alert").text("Not Hit");
-    
+      if (result_1 > "30" && result_1 < "50") {
+        $("#alert").text("Detected Sensor 1")
+        $("#detection").removeClass("card-light")
+        $("#alert").css("color", "white")
+        $("#detection").addClass("card-success")
       }
-      // setTimeout(function() {
-      //   sensor_1()
-      // }, 50)
-    },
-  })
-  $("#sensor_1_val").text(result_1)
-// }
-// sensor_1()
+      if (
+        result_1 > "30" &&
+        result_1 < "50" &&
+        (result > "30" && result < "50")
+      ) {
+        $("#alert").text("Detected on Both Sensors")
+        $("#detection").removeClass("card-light")
+        $("#alert").css("color", "white")
+        $("#detection").addClass("card-success")
+      }
+    } else {
+      $("#alert").text("not Detected")
+      $("#detection").removeClass("card-success")
+      $("#alert").css("color", "white")
+      $("#detection").addClass("card-light")
+    }
+    var a = parseInt(result)
+    var b = parseInt(result_1)
+    var c = a + b
+    $("#distance").text(c)
+  }
 
-// function sensor_2() {
-
-  var sensor_2 = $("#sensor_2_val").val()
-
-  var result = null
-  //  var scriptUrl = "somefile.php?name=" + name;
-  $.ajax({
-    url: "curl.php",
-    type: "get",
-    // dataType: "html",
-    async: false,
-    data: { pin: "V4", method: "get" },
-    success: function(data_2) {
-      result = data_2
-      // setTimeout(function() {
-      //   sensor_2()
-      // }, 100)
-    },
-    
-  })
-  $("#sensor_2_val").text(result)
-// }
-// sensor_2()
-setTimeout(function() {
   send()
-}, 200)
-  // }
-  // $("#alert").text("Hit");
-
-
-      // var sensor_1= data_1;
-
-}
-send()
-
-// else{
-//   return(0);
-// }
-
- 
 })
